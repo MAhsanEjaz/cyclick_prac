930ab731a28423835c5f2609697c832656e8359a
M.AHSAN <ahsanejaz99@gmail.com>
2023-08-05 13:21:11 +0500

    uploadingData

--- router/routes.js
+++ router/routes.js
@@ -2,7 +2,42 @@ const express = require('express');
 
 const userModel = require('../model/user_model');
 
+const imageModel = require('../model/image_model');
+
+const multer = require('multer');
+
 const app = express();
+
+
+app.use(express.static('uploads'));
+
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+      cb(null, 'uploads/');
+    },
+    filename: function(req, file, cb){
+     cb(null, Date.now() + '-' + file.originalname);
+    }
+    })
+    
+    const upload = multer({
+      storage:storage
+    });
+
+
+    
+    app.post('/file',upload.single('myImage'),async(req,res)=>{
+
+
+        const {name} = req.body;
+        const image = req.file.filename;
+        const data = imageModel({
+            name:name,
+            image:image});
+       await data.save();
+        res.json(data);
+    });
 
 
 app.post('/postData', async (req, res) => {
